42b6509467a3030af40f61446ae5015912990df1
Marco Aurélio de Souza Andrade Mesquita <141258978+marcoaurelio92@users.noreply.github.com>
2023-11-24 15:36:12 -0300

    Módulo F Aula 16
    Funções.
     Chamada > Parâmetro > Ação > Retorno.

---
+++ Curso em Vídeo/Curso JS/Exercícios/ex018/aula016-02.js
@@ -0,0 +1,11 @@
+/*
+function soma(n1, n2){
+    return n1+n2
+}
+console.log(soma(15, 19))
+*/
+
+function soma(n1=0, n2=0){
+    return n1+n2
+}
+console.log(soma(7))
---
+++ Curso em Vídeo/Curso JS/Exercícios/ex019/aula016-03.js
@@ -0,0 +1,4 @@
+var dobro = function dob(x){
+    return x*2
+}
+console.log(dobro(9))
